3cff73736d5b8762fbc60f82a24a92cf268d2b45
Кулик Роман <supervodka666@outlook.com>
2022-04-19 19:22:59 +0300

    6 cases

--- BCAW/BCAW/Offer.cs
+++ BCAW/BCAW/Offer.cs
@@ -4,6 +4,8 @@ namespace BCAW
 {
     public  class Offer
     {
+        public int UserId { get; set; }
+        public int Id { get; set; }
         public string? Experience { get; set; } 
         public string? Job { get; set; }
         public string? Reward { get; set; }
--- BCAW/BCAW/Program.cs
+++ BCAW/BCAW/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BCAW;
+using System.Collections.Generic;
 
 do
 {
@@ -7,14 +8,13 @@ do
 
 void AskUser()
 {
-   
     Console.WriteLine("Что вы хотите?");
     Console.WriteLine("1 - Добавить пользователя");
     Console.WriteLine("2 - Посчитай мне пользователей");
     Console.WriteLine("3 - Добавить анкету");
     Console.WriteLine("4 - Поиск анкеты");
     Console.WriteLine("5 - Отобразить все анкеты");
-    Console.WriteLine("6 - Принять предложение");
+    Console.WriteLine("6 - Взять заказ");
     Console.WriteLine("7 - Создать комментарий к предложению");
     Console.WriteLine("8 - Редактирование аккаунта");
   
@@ -25,6 +25,9 @@ void AskUser()
         case 1: AddUser(); break;
         case 2: CountUser(); break;
         case 3: AddOffer(); break;
+        case 4: SearchOffer(); break;
+        case 5:CheckAllOffers(); break;
+        case 6: ConfirmOffer(); break;
         default:
             Console.WriteLine("Введите корректное число");
             break ; 
@@ -44,6 +47,7 @@ void AddUser()
     var user = new User();
     user.Name = name;
     user.Age = age;
+    user.Id = Storage.Users.Count;
     Storage.Users.Add(user);
 
 }
@@ -58,13 +62,42 @@ void CountUser()
         Console.WriteLine($"количество пользователей : {Storage.Users.Count}");
         foreach (var user in Storage.Users)
         {
-            Console.WriteLine(user.Name);
-            Console.WriteLine(user.Age);
+            Console.WriteLine($"user name:{user.Name}");
+            Console.WriteLine($"user Age :{user.Age}");
         }
         Console.ReadKey();
     }
-   
 }
+int ChooseUser()
+{
+    Console.WriteLine("Выберите пользователя");
+    DrawUsers();
+    int userId = Convert.ToInt32(Console.ReadLine());
+    if (userId <= Storage.Users.Count)
+    {
+        return userId;
+    }
+    else {
+        Console.WriteLine("Вы выбрали несуществующего пользователя");
+        var a = ChooseUser();
+        return a;
+    }
+}
+   
+
+void DrawUsers()
+{
+    foreach (var user in Storage.Users)
+        DrawUser(user);
+}
+void DrawUser(User user)
+{
+    Console.WriteLine($"user id:{user.Id}");
+    Console.WriteLine($"user name:{user.Name}");
+    Console.WriteLine($"user Age :{user.Age}");
+    Console.WriteLine();
+}
+
 void AddOffer()
 {
     Console.Clear();
@@ -86,12 +119,49 @@ void AddOffer()
         offer.Reward = Console.ReadLine();
         Console.WriteLine("Пару слов о себе");
         offer.AFewWords = Console.ReadLine();
-
+        offer.Id = Storage.Offers.Count;
+        offer.UserId = ChooseUser();
+        Storage.Offers.Add(offer);
     }
-
-
-
-
-
 }
-
+//вызывает метод отрисовки всех анкет по отдельности
+void CheckAllOffers()
+{
+    DrawOffers(Storage.Offers);
+}
+//делаем поиск по всем анкетам и передаём в новую коллекцию result
+void SearchOffer()
+{
+    Console.WriteLine("Введите критерии поиска");
+    var offerSearch = Console.ReadLine();
+    var result = Storage.Offers
+        .Where(offer => 
+            offer.Job.Contains(offerSearch))
+        .ToList();
+    DrawOffers(result);//вызывает DrawOffers и передает в него параметр являющийся коллекцией
+}
+// приниммет входящую коллекцию и делает  перебор обьектов и отрисовывает методом DrawOffer каждый по очереди
+void DrawOffers(List<Offer> offerCollection)
+{
+    foreach (var offer in offerCollection)
+    {
+        DrawOffer(offer);
+    }
+}
+//отрисовка полей-данных анкеты
+void DrawOffer(Offer offer)
+{
+    Console.WriteLine($"offer id:{offer.Id}");
+    Console.WriteLine($"Job:{offer.Job}");
+    Console.WriteLine($"Experience:{offer.Experience}");
+    Console.WriteLine($"Reward:{offer.Reward}");
+    Console.WriteLine($"A fer words:{offer.AFewWords}");
+    Console.WriteLine();
+}
+void ConfirmOffer()
+{
+    Console.WriteLine("Выберите заказ");
+    DrawOffers(Storage.Offers);
+    int confirmedOfferId = Convert.ToInt32(Console.ReadLine());
+    Storage.Offers.RemoveAt(confirmedOfferId);
+}
